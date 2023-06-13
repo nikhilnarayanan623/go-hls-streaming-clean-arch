704401d4529170017c0893f8f2c8cae609d10feb
nikhil623 <nikhilnarayanan623@gmail.com>
2023-06-13 20:38:48 +0530

    video playlist creation in usecase go routine added

--- pkg/domain/video.go
+++ pkg/domain/video.go
@@ -3,11 +3,11 @@ package domain
 import "time"
 
 type Video struct {
-	ID          string        `json:"id" gorm:"primaryKey;not null"`
-	Name        string        `json:"name" gorm:"not null"`
-	VideoUrl    string        `json:"video_url" gorm:"not null"`
-	PlaylistUrl string        `json:"playlist_url" gorm:"not null"`
-	Description string        `json:"description" gorm:"not null"`
+	ID          string `json:"id" gorm:"primaryKey;not null"`
+	Name        string `json:"name" gorm:"not null"`
+	VideoUrl    string `json:"video_url" gorm:"not null"`
+	PlaylistUrl string `json:"playlist_url" gorm:"not null"`
+	Description string `json:"description" gorm:"not null"`
 	//Duration    time.Duration `json:"duration" gorm:"not null"`
-	UploadedAt  time.Time     `json:"uploaded_at" gorm:"not null"`
+	UploadedAt time.Time `json:"uploaded_at" gorm:"not null"`
 }
